b6076696868867dd8d5ca4edea5304fead9db2a7
tkoibaev <tam.koib12@gmail.com>
2024-01-29 01:20:39 +0300

    initial configuration

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,5 +1,4 @@
 import React from "react"
-import styles from "./Button.module.scss"
 
 const Button = () => {
   return <div>Button</div>
--- src/components/DropDown/DropDown.tsx
+++ src/components/DropDown/DropDown.tsx
@@ -1,5 +1,6 @@
 import React from "react"
 import styles from "./DropDown.module.scss"
+import Button from "components/Button"
 
 const DropDown = () => {
   return <div>DropDown</div>
