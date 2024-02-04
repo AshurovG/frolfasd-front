c6923809328328651d95dc80677a8d34faa063eb
Georgy <ashurovgeorgy@MacBook-Pro-som-tillhor-Georgy.local>
2024-02-04 16:31:48 +0300

    edit navigation component

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -2,15 +2,15 @@ import React from 'react'
 import styles from './Navigation.module.scss'
 
 export type MultiDropdownProps = {
-    onFacadesClick?: () => void;
+    onFacadesClick: () => void;
     onImportantFacades?: () => void;
     onQuestionsClick?: () => void;
 };
 
-const Navigation: React.FC<MultiDropdownProps> = () => {
+const Navigation: React.FC<MultiDropdownProps> = ({onFacadesClick}) => {
   return (
     <div  className={styles.nav}>
-        <div className={styles['nav__item']}>Вентилируемые фасады</div>
+        <div onClick={() => onFacadesClick()} className={styles['nav__item']}>Вентилируемые фасады</div>
         <div className={styles['nav__item']}>Вентилируемые фасады на главной странице</div>
         <div className={styles['nav__item']}>Часто задаваемые вопросы</div>
     </div>
--- src/pages/AdminPage/AdminPage.tsx
+++ src/pages/AdminPage/AdminPage.tsx
@@ -1,17 +1,27 @@
 import React from 'react'
 import styles from './AdminPage.module.scss'
 import Navigation from 'components/Navigation'
+import axios from 'axios'
 
 const AdminPage = () => {
   React.useEffect(() => {
     console.log('render')
   }, [])
 
+  const getFacades = async () => {
+    try {
+      const response = await axios('https://frolfasd.ru/api/exterior_design/')
+      console.log(response)
+    } catch {
+
+    }
+  }
+
   return (
     <div className={styles.admin}>
       <h1 className={styles.admin__title}>Управление сайтом</h1>
       <h4 className={styles.admin__subtitle}>Здесь вы можете редактировать данные сайта</h4>
-      <Navigation/>
+      <Navigation onFacadesClick={getFacades}/>
     </div>
   )
 }
